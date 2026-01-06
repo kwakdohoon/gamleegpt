b3f2cb99f921bd1ae1c0f41c94094510dd548089
kwakdohoon <kwakdohoon@gmail.com>
2026-01-06 18:49:49 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,7 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-import path from "path";
-import { componentTagger } from "lovable-tagger";
-
-
-import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  plugins: [react()],
   base: "/gamleegpt/",
 });
-
