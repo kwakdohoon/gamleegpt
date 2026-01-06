af0a68e671193a9df2d8af84ec735c5e60e9126f
kwakdohoon <kwakdohoon@gmail.com>
2026-01-06 23:11:57 +0900

    찐막1

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path' // 추가
 
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'), // @를 src 폴더로 연결
+    },
+  },
 })
